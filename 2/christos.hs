8c4bfb9a4f3e22ce4eb941a4f5519ed9d7561f36
Christos Ioannidis <christos.ioannidis@stud.uni-bamberg.de>
2017-12-03 16:16:57 +0100

    day 2 part 2

--- 2/christos.hs
+++ 2/christos.hs
@@ -1,17 +1,30 @@
 import Data.Char
 
-
 main ::  IO()
 main = do
     filecontent <- readFile "inputChristos.txt"
-    let result = sum $ map calculateLines $ lines filecontent
-    print result
+    let result1 = sum $ map calculateLines $ lines filecontent
+    let result2 = sum $ map calculateLines2 $ lines filecontent
+    print result1
+    print result2
     
-isPalindrome w = w==reverse w
 
 calculateLines :: String -> Int
 calculateLines [] = 0
 calculateLines xs = (getMax $ parseStringToIntList $ words xs ) - (getMin $ parseStringToIntList $ words xs)
+
+calculateLines2 :: String -> Int
+calculateLines2 [] = 0
+calculateLines2 xs = getDivision $ parseStringToIntList $ words xs
+
+getDivision :: [Int] -> Int 
+getDivision [] = 0
+getDivision (x:[]) = 0
+getDivision xs = getMax(([(isDivisible a b) | a <- xs, b<-xs, a /= b && mod a b == 0]))
+
+isDivisible :: Int -> Int -> Int
+isDivisible a b   | a `mod` b == 0 = a `div` b
+                    | otherwise = 0
 
 parseStringToIntList :: [String] -> [Int]
 parseStringToIntList xs = map (read) xs 

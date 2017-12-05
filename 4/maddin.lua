2cf6b0b1ac63885915fed3e4ba29a9d36db9f925
Martin Endress <martin.endress@stud.uni-bamberg.de>
2017-12-05 11:49:17 +0100

    second part

--- 4/maddin.lua
+++ 4/maddin.lua
@@ -1,4 +1,29 @@
-f = function (pass)
+sortedStr = function(strIn)
+  retArray = { }
+  for i = 1, #strIn do
+    local c = strIn:sub(i,i)
+    retArray[i] = c
+  end
+  table.sort(retArray)
+  return retArray
+end
+
+eq = function (e1,e2) return (e1==e2) end
+isAnagram = function (e1,e2)
+  if(string.len(e1)~=string.len(e2)) then
+    return false
+  end
+  sortedE1 = sortedStr(e1)
+  sortedE2 = sortedStr(e2)
+  for i = 1,table.getn(sortedE1),1 do
+    if(sortedE1[i] ~= sortedE2[i]) then
+      return false
+    end
+  end
+  return true
+end
+
+containsMatch = function (func,pass)
   local list = string.gmatch(pass, "%S+")
 
   array = {}
@@ -9,7 +34,7 @@ f = function (pass)
   length = table.getn(array)
   for i=1,length,1 do
     for j=1,length,1 do
-      if (j ~= i) and (array[i] == array[j]) then
+      if (j ~= i) and func(array[i],array[j]) then
         return 0
       end
     end
@@ -25,13 +50,18 @@ function map(func, array)
   return new_array
 end
 
-local inStr = {
-    "beispiel valider passphrase",
-    "beispiel invalid , da invalid"}
-local result = map(f,inStr)
+function curry(f)
+  return function (x) return function (y) return f(x,y) end end
+end
+
+local inStr = {"abc cba","acb acbd"}
+local result = map(curry(containsMatch) (isAnagram),inStr)
+
+-- TODO replace this with a reduce
 local sum = 0
 for i = 1,table.getn(result),1 do
   sum = sum + result[i]
 end
+--
 
 print(sum)
